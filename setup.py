5991120e9d98dcaddc5606fce39f2446fc4dfdc4
rfigueroa <unnickchido@gmail.com>
2016-08-07 10:44:58 -0500

    fix imports

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'stream2chromecast',
   packages = ['stream2chromecast'], # this must be the same as the name above
-  version = '0.1',
+  version = '0.1.1',
   description = 'Chromecast media streamer',
   author = 'rfigueroa',
   author_email = 'rfigueroaoficial@gmail.com',
   url = 'https://github.com/rfigueroa/stream2chromecast',
-  download_url = 'https://github.com/rfigueroa/stream2chromecast/tarball/0.1',
+  download_url = 'https://github.com/rfigueroa/stream2chromecast/tarball/0.1.1',
   keywords = ['Chromecast', 'streamer' ],
   classifiers = [],
 )
